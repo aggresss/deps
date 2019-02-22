62f4f2be71c51c2f3af466ef00d38ede6c25e682
aggresss <aggresss@163.com>
2019-02-22 20:49:57 +0800

    2019-02-22 20:49:56 CST W07D5

--- b64/b64_test.c
+++ b64/b64_test.c
@@ -16,14 +16,17 @@
 int main(void)
 {
     size_t out_len = 1024;
+    char out_buf[1024] = {0};
     char data[] = "https://github.com/aggresss/deps/blob/master/b64/README.md";
-    char buff[1024] = {0};
     char expect_b64_string[] = "aHR0cHM6Ly9naXRodWIuY29tL2FnZ3Jlc3NzL2RlcHMvYmxvYi9tYXN0ZXIvYjY0L1JFQURNRS5tZA==";
 
-    out_len = urlsafe_b64_encode(data, strlen(data), buff, out_len);
-    if (out_len != strlen(expect_b64_string))
+    out_len = urlsafe_b64_encode(data, strlen(data), out_buf, out_len);
+    printf("out length: %zu\n", out_len);
+    printf("out buffer: %s\n", out_buf);
+    if (out_len != strlen(expect_b64_string)) {
         exit(EXIT_FAILURE);
-    if (0 != strncmp(buff, expect_b64_string, strlen(expect_b64_string)))
+    }
+    if (0 != strncmp(out_buf, expect_b64_string, strlen(expect_b64_string)))
         exit(EXIT_FAILURE);
 
     return EXIT_SUCCESS;
