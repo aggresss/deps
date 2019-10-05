8ba7dd359cb1a9f30ff1c928c4bde03152d1e0c2
aggresss <aggresss@163.com>
2019-10-05 14:17:55 +0800

    2019-10-05 14:17:55 CST W39D6

--- rbtree/rbtree_test.c
+++ rbtree/rbtree_test.c
@@ -5,150 +5,16 @@
 #include <time.h>
 
 
-int check(Tree *t)
+int main(int argc, const char * argv[])
 {
-    Node* curnode = NULL;
     int rc = 0;
-
-    curnode = TreeNextElement(t, curnode);
-    while (curnode) {
-        Node* prevnode = curnode;
-
-        curnode = TreeNextElement(t, curnode);
-
-        if (prevnode && curnode && (*(int*) (curnode->content) < *(int*) (prevnode->content))) {
-            printf("out of order %d < %d\n", *(int*) (curnode->content), *(int*) (prevnode->content));
-            rc = 99;
-        }
-    }
-    return rc;
-}
-
-int traverse(Tree *t, int lookfor)
-{
-    Node* curnode = NULL;
-    int rc = 0;
-
-    printf("Traversing\n");
-    curnode = TreeNextElement(t, curnode);
-    /* printf("content int %d\n", *(int*)(curnode->content)); */
-    while (curnode) {
-        Node* prevnode = curnode;
-
-        curnode = TreeNextElement(t, curnode);
-        /* if (curnode)
-         printf("content int %d\n", *(int*)(curnode->content)); */
-        if (prevnode && curnode && (*(int*) (curnode->content) < *(int*) (prevnode->content))) {
-            printf("out of order %d < %d\n", *(int*) (curnode->content), *(int*) (prevnode->content));
-        }
-        if (curnode && (lookfor == *(int*) (curnode->content)))
-            printf("missing item %d actually found\n", lookfor);
-    }
-    printf("End traverse %d\n", rc);
-    return rc;
-}
-
-int test(int limit)
-{
-    int i, *ip, *todelete;
+    int *ip = NULL;
     Tree* t = TreeInit(TreeIntCompare);
-    int rc = 0;
 
-    printf("Tree initialized\n");
 
-    srand(time(NULL));
 
     ip = malloc(sizeof(int));
     *ip = 2;
-    TreeAdd(t, (void*) ip, sizeof(int));
-
-    check(t);
-
-    i = 2;
-    void* result = TreeRemove(t, (void*) &i);
-    if (result)
-        free(result);
-
-    int actual[limit];
-    for (i = 0; i < limit; i++) {
-        void* replaced = NULL;
-
-        ip = malloc(sizeof(int));
-        *ip = rand();
-        replaced = TreeAdd(t, (void*) ip, sizeof(int));
-        if (replaced) /* duplicate */
-        {
-            free(replaced);
-            actual[i] = -1;
-        } else
-            actual[i] = *ip;
-        if (i == 5)
-            todelete = ip;
-        printf("Tree element added %d\n", *todelete);
-        if (1 % 1000 == 0) {
-            rc = check(t);
-            printf("%d elements, check result %d\n", i + 1, rc);
-            if (rc != 0)
-                return 88;
-        }
-    }
-
-    check(t);
-
-    for (i = 0; i < limit; i++) {
-        int parm = actual[i];
-
-        if (parm == -1)
-            continue;
-
-        Node* found = TreeFind(t, (void*) &parm);
-        if (found)
-            printf("Tree find %d %d\n", parm, *(int*) (found->content));
-        else {
-            printf("%d not found\n", parm);
-            traverse(t, parm);
-            return -2;
-        }
-    }
-
-    check(t);
-
-    for (i = limit - 1; i >= 0; i--) {
-        int parm = actual[i];
-        void *found;
-
-        if (parm == -1) /* skip duplicate */
-            continue;
-
-        found = TreeRemove(t, (void*) &parm);
-        if (found) {
-            printf("%d Tree remove %d %d\n", i, parm, *(int*) (found));
-            free(found);
-        } else {
-            int count = 0;
-            printf("%d %d not found\n", i, parm);
-            traverse(t, parm);
-            for (i = 0; i < limit; i++)
-                if (actual[i] == parm)
-                    ++count;
-            printf("%d occurs %d times\n", parm, count);
-            return -2;
-        }
-        if (i % 1000 == 0) {
-            rc = check(t);
-            printf("%d elements, check result %d\n", i + 1, rc);
-            if (rc != 0)
-                return 88;
-        }
-    }
-    printf("finished\n");
-    return 0;
+    replaced = TreeAdd(t, (void*) ip, sizeof(int));
 }
 
-int main(int argc, char *argv[])
-{
-    int rc = 0;
-
-    while (rc == 0)
-        rc = test(999999);
-}
